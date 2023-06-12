8ce6530e8763810bae72805a817463f0265a5c28
Julie Shapovalenko <jools.shapo@gmail.com>
2023-06-12 14:43:30 +0300

    delete clg

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,24 +1,15 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { logOut } from 'redux/auth/authOperations';
-import {
-  selectIsLoggedIn,
-  selectIsRefreshing,
-  selectUser,
-} from 'redux/selectors';
+import { selectUser } from 'redux/selectors';
 import { Button, Name, Wrapper } from './UserMenu.styled';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
-  console.log(user);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  console.log(isLoggedIn);
-  const isRefreshing = useSelector(selectIsRefreshing);
-  console.log(isRefreshing);
 
   return (
     <Wrapper>
-      {!isRefreshing && isLoggedIn && <Name>Welcome,{user.name} </Name>}
+      <Name>Welcome,{user.name} </Name>
       <Button type="button" onClick={() => dispatch(logOut())}>
         Log Out
       </Button>
